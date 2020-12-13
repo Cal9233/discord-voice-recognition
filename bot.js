a76d0ef382d870eb257cd8d77dacad69b9c6b2a9
Cal <calvin.m71@hotmail.com>
2020-12-13 15:27:57 -0800

    connected

--- bot.js
+++ bot.js
@@ -3,18 +3,21 @@ require("dotenv").config()
 const { error } = require("console");
 const Discord = require('discord.js');
 const fs = require('fs');
+const queue = new Map();
 
 //Create a PCM audio
-const audio = connection.receiver.createStream(user, {node: "pcm"});
+// const audio = connection.receiver.createStream(user, {node: "pcm"});
 
-audio.pipe(fs.createWriteStream('user_audio'));
+// audio.pipe(fs.createWriteStream('user_audio'));
 
 //Bot commands
 const anti_venom = new Discord.Client();
 
+
 anti_venom.login(process.env.BOT_TOKEN)
 
-anti_venom.on('ready', () => {
+anti_venom.on('ready', user => {
+  
   try {
     console.log('Symbiote Online')
   } catch (e) {
@@ -22,11 +25,27 @@ anti_venom.on('ready', () => {
   }
 });
 
+
 anti_venom.on('message', msg => {
   if (msg.content === 'Anti-Venom') {
     msg.channel.send("We are here")
-  } else {
-    console.log(error.toString())
   }
 });
 
+//Connecting the bot to voice channel
+anti_venom.on('message', async(msg) => {
+  const prefix = '?';
+
+  if (msg.content === `${prefix}Connect`) {
+    /// Get the voice channel that the user is in
+    const vc = msg.member.voice.channel;
+    ///Check if voice channel exists
+    if (!vc) return message.channel.send('User is not in a channel i can connect to.')
+    ///join the voice channel
+    vc.join()
+    .then(connection => console.log('connected!'))
+    .catch(err => {
+      console.log(err)
+  })
+  }
+});
